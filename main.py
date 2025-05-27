a2d9a9d69a5168aea50bcd561d3e585a238e3afa
lcavarge <leonardo.cavarge@h2grupo.com>
2025-05-27 19:21:31 -0300

    Update main.py

--- main.py
+++ main.py
@@ -124,4 +124,5 @@ async def slack_events(req: Request):
     return {"ok": True}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 10000))
+uvicorn.run("main:app", host="0.0.0.0", port=port)
